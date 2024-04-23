6e3c9ce29d01861b56e8bec585b209e6a08a7185
Boaz <webevolution13@gmail.com>
2024-04-23 17:43:32 +0200

    upDate
    front end

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,38 @@
-<script setup>
+<script setup lang="ts">
 import { ref, computed } from 'vue'
 
-const items = ref([
-  { title: 'Elem 1', description: 'Ez az első elem leírása.' },
-  { title: 'Elem 2', description: 'Ez a második elem leírása.' },
-  { title: 'Elem 3', description: 'Ez a harmadik elem leírása.' },
+const sliderElem = ref([
+  {
+    title: 'KIZÁRÓLAG PRÉMIUM ALAPANYOGOKBÓL DOLGOZUNK',
+    description:
+      'Minden termékünket kizárólag első osztályú szövetek felhasználásával készítjük, így garantálva otthona számára a stílus és minőség tökéletes harmóniáját. Válasszon boltunkat, ahol a kifinomultság és tartósság találkozik, hogy otthonát a lehető legjobban kiegészíthesse.',
+    button: 'TOVÁBB',
+  },
+  {
+    title: 'KIZÁRÓLAG PRÉMIUM ALAPANYOGOKBÓL DOLGOZUNK',
+    description:
+      'Minden termékünket kizárólag első osztályú szövetek felhasználásával készítjük, így garantálva otthona számára a stílus és minőség tökéletes harmóniáját. Válasszon boltunkat, ahol a kifinomultság és tartósság találkozik, hogy otthonát a lehető legjobban kiegészíthesse.',
+    button: 'TOVÁBB',
+  },
+  {
+    title: 'KIZÁRÓLAG PRÉMIUM ALAPANYOGOKBÓL DOLGOZUNK',
+    description:
+      'Minden termékünket kizárólag első osztályú szövetek felhasználásával készítjük, így garantálva otthona számára a stílus és minőség tökéletes harmóniáját. Válasszon boltunkat, ahol a kifinomultság és tartósság találkozik, hogy otthonát a lehető legjobban kiegészíthesse.',
+    button: 'TOVÁBB',
+  },
 ])
+
+const highlightPremium = (text: string | null | undefined): string => {
+  if (!text) {
+    return ''
+  }
+  return text.replace('PRÉMIUM', '<strong class="f-600">PRÉMIUM</strong>')
+}
 
 const currentSlide = ref(0)
 
 const nextSlide = () => {
-  if (currentSlide.value < items.value.length - 1) {
+  if (currentSlide.value < sliderElem.value.length - 1) {
     currentSlide.value++
   } else {
     currentSlide.value = 0
@@ -21,7 +43,7 @@ const prevSlide = () => {
   if (currentSlide.value > 0) {
     currentSlide.value--
   } else {
-    currentSlide.value = items.value.length - 1
+    currentSlide.value = sliderElem.value.length - 1
   }
 }
 
@@ -51,28 +73,50 @@ const decrement = () => {
 <template>
   <div>
     <section>
-      <div class="slider-content">
+      <div class="slider-content position-relative">
         <NuxtImg class="slider-content__img" src="/img/slider/slider.webp" />
-        <div class="carousel-container">
-          <div
-            class="carousel-slide"
-            :style="{ transform: `translateX(-${currentSlide * 100}%)` }"
-          >
+        <div class="slider-content__carousel-content position-absolute">
+          <div class="carousel position-relative">
             <div
-              class="carousel-item"
-              v-for="(item, index) in items"
-              :key="index"
+              class="carousel-slide d-flex"
+              :style="{ transform: `translateX(-${currentSlide * 100}%)` }"
             >
-              <h3>{{ item.title }}</h3>
-              <p>{{ item.description }}</p>
+              <div
+                class="carousel-item"
+                v-for="(item, index) in sliderElem"
+                :key="index"
+              >
+                <h1
+                  class="carousel-item__h1 text-color f-300"
+                  v-html="highlightPremium(item.title)"
+                ></h1>
+                <p class="carousel-item__p text-color f-400">
+                  {{ item.description }}
+                </p>
+                <NuxtLink class="carousel-item__NuxtLink f-300">{{
+                  item.button
+                }}</NuxtLink>
+              </div>
             </div>
+            <button
+              class="carousel-control position-absolute carousel-control--left"
+              @click="prevSlide"
+            >
+              <NuxtImg
+                class="slider-content__img"
+                src="/img/slider/a-left.svg"
+              />
+            </button>
+            <button
+              class="carousel-control position-absolute carousel-control--right"
+              @click="nextSlide"
+            >
+              <NuxtImg
+                class="slider-content__img"
+                src="/img/slider/a-right.svg"
+              />
+            </button>
           </div>
-          <button class="carousel-control left" @click="prevSlide">
-            &#10094;
-          </button>
-          <button class="carousel-control right" @click="nextSlide">
-            &#10095;
-          </button>
         </div>
       </div>
     </section>
