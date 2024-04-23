96de6eceb10e148cbb4e5444e5e23a706e63a59f
Boaz <webevolution13@gmail.com>
2024-04-23 15:15:41 +0200

    upDate
    front end

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,30 @@
 <script setup>
 import { ref, computed } from 'vue'
 
+const items = ref([
+  { title: 'Elem 1', description: 'Ez az első elem leírása.' },
+  { title: 'Elem 2', description: 'Ez a második elem leírása.' },
+  { title: 'Elem 3', description: 'Ez a harmadik elem leírása.' },
+])
+
+const currentSlide = ref(0)
+
+const nextSlide = () => {
+  if (currentSlide.value < items.value.length - 1) {
+    currentSlide.value++
+  } else {
+    currentSlide.value = 0
+  }
+}
+
+const prevSlide = () => {
+  if (currentSlide.value > 0) {
+    currentSlide.value--
+  } else {
+    currentSlide.value = items.value.length - 1
+  }
+}
+
 const quantity = ref(200)
 
 const quantityDisplay = computed(() => {
@@ -29,8 +53,30 @@ const decrement = () => {
     <section>
       <div class="slider-content">
         <NuxtImg class="slider-content__img" src="/img/slider/slider.webp" />
+        <div class="carousel-container">
+          <div
+            class="carousel-slide"
+            :style="{ transform: `translateX(-${currentSlide * 100}%)` }"
+          >
+            <div
+              class="carousel-item"
+              v-for="(item, index) in items"
+              :key="index"
+            >
+              <h3>{{ item.title }}</h3>
+              <p>{{ item.description }}</p>
+            </div>
+          </div>
+          <button class="carousel-control left" @click="prevSlide">
+            &#10094;
+          </button>
+          <button class="carousel-control right" @click="nextSlide">
+            &#10095;
+          </button>
+        </div>
       </div>
     </section>
+
     <section>
       <div class="products-content">
         <h2
@@ -40,7 +86,12 @@ const decrement = () => {
         </h2>
         <div class="counter d-flex">
           <button class="counter__btn" @click="decrement">-</button>
-          <input class="counter__input" type="text" :value="quantityDisplay" readonly />
+          <input
+            class="counter__input"
+            type="text"
+            :value="quantityDisplay"
+            readonly
+          />
           <button class="counter__btn" @click="increment">+</button>
         </div>
       </div>
