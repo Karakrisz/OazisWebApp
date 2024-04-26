d481cadb84c41bbdcd650fe985cbf0b053e1f661
Boaz <webevolution13@gmail.com>
2024-04-27 01:09:29 +0200

    upDate
    front end

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -4,73 +4,78 @@
   <footer>
     <div class="footer-content grid-4">
       <div class="footer-content__link-box">
-        <h6 class="footer-content__link-box__h6">MENÜ</h6>
+        <h6 class="footer-content__link-box__h6 text-color">MENÜ</h6>
         <p class="footer-content__link-box__p">
-          <NuxtLink class="footer-content__link-box__NuxtLink"
+          <NuxtLink class="footer-content__link-box__NuxtLink text-color"
             >TERMÉKEK</NuxtLink
           >
         </p>
         <p class="footer-content__link-box__p">
-          <NuxtLink class="footer-content__link-box__NuxtLink"
+          <NuxtLink class="footer-content__link-box__NuxtLink text-color"
             >BELÉPÉS</NuxtLink
           >
         </p>
         <p class="footer-content__link-box__p">
-          <NuxtLink class="footer-content__link-box__NuxtLink"
+          <NuxtLink class="footer-content__link-box__NuxtLink text-color"
             >REGISZTRÁCIÓ</NuxtLink
           >
         </p>
         <p class="footer-content__link-box__p">
-          <NuxtLink class="footer-content__link-box__NuxtLink"
+          <NuxtLink class="footer-content__link-box__NuxtLink text-color"
             >PROFILOM</NuxtLink
           >
         </p>
         <p class="footer-content__link-box__p">
-          <NuxtLink class="footer-content__link-box__NuxtLink">KOSÁR</NuxtLink>
+          <NuxtLink class="footer-content__link-box__NuxtLink text-color"
+            >KOSÁR</NuxtLink
+          >
         </p>
         <p class="footer-content__link-box__p">
-          <NuxtLink class="footer-content__link-box__NuxtLink"
+          <NuxtLink class="footer-content__link-box__NuxtLink text-color"
             >PROFILOM</NuxtLink
           >
         </p>
       </div>
-      <div class="footer-content__link-box">
-        <h6 class="footer-content__link-box__h6">INFORMÁCIÓK</h6>
+      <div class="footer-content__link-box footer-content__link-box--one-formating">
+        <h6 class="footer-content__link-box__h6 text-color">INFORMÁCIÓK</h6>
         <p class="footer-content__link-box__p">
-          <NuxtLink class="footer-content__link-box__NuxtLink"
+          <NuxtLink class="footer-content__link-box__NuxtLink text-color"
             >ÁLTALÁNOS SZERZŐDÉSI FELTÉTELEK</NuxtLink
           >
         </p>
         <p class="footer-content__link-box__p">
-          <NuxtLink class="footer-content__link-box__NuxtLink"
+          <NuxtLink class="footer-content__link-box__NuxtLink text-color"
             >ADATKEZELÉSI TÁJÉKOZTATÓ</NuxtLink
           >
         </p>
         <p class="footer-content__link-box__p">
-          <NuxtLink class="footer-content__link-box__NuxtLink"
+          <NuxtLink class="footer-content__link-box__NuxtLink text-color"
             >FIZETÉS</NuxtLink
           >
         </p>
         <p class="footer-content__link-box__p">
-          <NuxtLink class="footer-content__link-box__NuxtLink"
+          <NuxtLink class="footer-content__link-box__NuxtLink text-color"
             >SZÁLLÍTÁS</NuxtLink
           >
         </p>
         <p class="footer-content__link-box__p">
-          <NuxtLink class="footer-content__link-box__NuxtLink"
+          <NuxtLink class="footer-content__link-box__NuxtLink text-color"
             >GYAKRAN ISMÉTELT KÉRDÉSEK</NuxtLink
           >
         </p>
         <p class="footer-content__link-box__p">
-          <NuxtLink class="footer-content__link-box__NuxtLink"
+          <NuxtLink class="footer-content__link-box__NuxtLink text-color"
             >ELÉRHETŐSÉGEK</NuxtLink
           >
         </p>
       </div>
-      <div class="footer-content__link-box">
-        <h6 class="footer-content__link-box__h6">ELÉRHETŐSÉG</h6>
+      <div class="footer-content__link-box footer-content__link-box--two-formating">
+        <h6 class="footer-content__link-box__h6 text-color">ELÉRHETŐSÉG</h6>
         <p class="footer-content__link-box__p">
-          <a href="tel:+36704266794" class="footer-content__link-box__link">
+          <a
+            href="tel:+36704266794"
+            class="footer-content__link-box__link text-color"
+          >
             <NuxtImg
               class="footer-content__link-box__link__img"
               src="/img/footer/mobile.svg"
@@ -79,7 +84,7 @@
             +36 70 426 67 94</a
           >
         </p>
-        <p class="footer-content__link-box__p">
+        <p class="footer-content__link-box__p text-color">
           <NuxtImg
             class="footer-content__link-box__link__img"
             src="/img/footer/time.svg"
@@ -90,7 +95,7 @@
         <p class="footer-content__link-box__p">
           <a
             href="mailto:fuggonyoazis@gmail.com"
-            class="footer-content__link-box__link"
+            class="footer-content__link-box__link text-color"
           >
             <NuxtImg
               class="footer-content__link-box__link__img"
@@ -101,7 +106,7 @@
           >
         </p>
       </div>
-      <div class="footer-content__img-content">
+      <div class="footer-content__img-content d-flex">
         <NuxtImg
           class="footer-content__img-content__img"
           src="img/header/logo.svg"
